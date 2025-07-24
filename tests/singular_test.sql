9348f18fff3dcbc6436495828f8bf68fcaa9d686
Alex Viana <analystthealex@gmail.com>
2025-07-24 12:58:50 +0000

    added copy of overview on the root folder

--- tests/singular_test.sql
+++ tests/singular_test.sql
@@ -1,5 +1,4 @@
--- Refunds have a negative amount, so the total amount should always be >= 0.
--- Therefore return records where total_amount < 0 to make the test fail.
+
 select
     orders_id,
     sum(revenue) as total_revenue
